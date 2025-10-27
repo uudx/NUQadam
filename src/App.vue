8e407a0cf36d42020dea1db9d69fe065a2740ab4
RemoteDeer <aslanismakatov12@gmail.com>
2025-10-28 02:50:42 +0500

    Functional template for Contact Popover, Header and home page with header.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
+import Header from './components/Header.vue'
 </script>
 
 <template>
+  <Header></Header>
+
+  <main>
+    <p>My page content</p>
+  </main>
 </template>
 
 <style scoped>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,31 @@
 <script setup>
+import ContactPopover from './ContactPopover.vue'
 </script>
 
 <template>
+  <header class="bg-white dark:bg-gray-900">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between px-6 lg:px-8" aria-label="Global">
+      
+      <div class="flex lg:flex-1 items-center justify-between">
+        <a href="#" class="-m-1 p-1 flex items-center gap-x-2">
+          <span class="sr-only">NUQadam</span>
+          
+          <img class="h-6 w-auto dark:hidden" src="/vite.svg" alt="NUQadam Logo">
+          
+          <img class="h-6 w-auto hidden dark:block" src="/vite.svg" alt="NUQadam Logo">
+
+          <span class="font-semibold text-black dark:text-white">
+            NUQadam
+          </span>
+        </a>
+      </div>
+
+      <div>
+        <ContactPopover/>
+      </div>
+
+    </nav>
+  </header>
 </template>
 
 <style scoped>
