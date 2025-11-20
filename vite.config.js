b20ae91cd94feba860f4c92ffeadd535837c958a
RemoteDeer <aslanismakatov12@gmail.com>
2025-11-20 19:36:20 +0500

    Setup deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import svgLoader from 'vite-svg-loader'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/NUQadam/',
   plugins: [
     vue(),
     tailwindcss(),
