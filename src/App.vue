7c1e0eb64d6b93b27e365dcc4e88db3b6e9edf9a
RemoteDeer <aslanismakatov12@gmail.com>
2025-11-22 19:34:49 +0500

    Cleaning repo. Dist folder removed from main branch and renamed as docs. The path of logo images are changed

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,8 @@
 <script setup>
 import Layout from './components/layouts/Layout.vue'
-import { onMounted } from 'vue'
-
-onMounted(() => {
-  console.log('App mounted!')
-  console.log('Base URL:', import.meta.env.BASE_URL)
-})
 </script>
 
 <template>
-
-  <div id="app">
-    <router-link to="/">Home</router-link> | 
-    <router-link to="/teachers">Teachers</router-link>
-  </div>
   <Layout>
     <router-view/>
   </Layout>
