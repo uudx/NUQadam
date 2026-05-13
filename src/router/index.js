80d168d0f3830668f629827f2d01d31f9387256d
Ame雨 <155887302+uudx@users.noreply.github.com>
2026-05-13 19:04:53 +0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) return savedPosition
